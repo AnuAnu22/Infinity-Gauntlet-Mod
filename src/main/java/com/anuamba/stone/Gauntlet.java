92f9837068a943e0308e56d8d25cd7c4fb306c74
AnuAnu22 <129385032+AnuAnu22@users.noreply.github.com>
2024-04-08 23:35:33 +0600

    starting

---
+++ src/main/java/com/anuamba/stone/Gauntlet.java
@@ -0,0 +1,10 @@
+package com.anuamba.stone;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Gauntlet implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
